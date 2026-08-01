6695511c3fb9998165d046d3f3cb4909afc3304f
Alexander Zakharov <alexander.zakharov@monetis.io>
2026-08-01 19:40:39 +0200

    fix(@monetis-io/sanity-studio-core): dynamically import CLI client

--- src/apps/sanity-studio-core/src/utils/sanity/index.ts
+++ src/apps/sanity-studio-core/src/utils/sanity/index.ts
@@ -1,6 +1,5 @@
 import { type ClientConfig, createClient } from '@sanity/client';
 import groq from 'groq';
-import { getCliClient } from 'sanity/cli';
 
 import { PROJECT_ID, DATASET } from '@project/env';
 import { type Sanity } from '@project/types';
@@ -13,13 +12,17 @@ export const getCustomers = async (): Promise<Sanity.Document.Customer[]> => {
     dataset: DATASET,
     apiVersion: '2026-07-17',
     perspective: 'published',
-    withCredentials: true,
   } satisfies ClientConfig;
 
   const query = groq`*[_type == 'customer']`;
 
   if (isBrowser())
-    return createClient(configuration).fetch<Sanity.Document.Customer[]>(query);
+    return createClient({
+      ...configuration,
+      withCredentials: true,
+    }).fetch<Sanity.Document.Customer[]>(query);
 
-  return getCliClient(configuration).fetch<Sanity.Document.Customer[]>(query);
+  return (await import('sanity/cli'))
+    .getCliClient(configuration)
+    .fetch<Sanity.Document.Customer[]>(query);
 };
